5368207a463165f98fd89418c81e9e7d214b38a9
Karthik <msdhamodharan5@gmail.com>
2025-07-21 16:39:54 +0530

    simple controller

--- src/main/java/com/supportservice/supportservice/SupportserviceApplication.java
+++ src/main/java/com/supportservice/supportservice/SupportserviceApplication.java
@@ -2,10 +2,20 @@ package com.supportservice.supportservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SupportserviceApplication {
 
+	@RestController
+	public class HelloWorld {
+		@GetMapping("/hello")
+		public String helloWorld() {
+			return "Hello World";
+		}
+		
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SupportserviceApplication.class, args);
 	}
